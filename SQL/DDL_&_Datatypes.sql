cf74ce706c76c0492049b89385474a7329bb0747
Benjamin Petruzziello <benjamin.petruzziello@revature.com>
2021-03-30 15:53:23 -0400

    added SQL folder?

---
+++ SQL/DDL_&_Datatypes.sql
@@ -0,0 +1,43 @@
+--select now(); here's your first SQL query!
+
+--Data Definition Language (DDL): create, alter, truncate, drop
+--let's create our first table using the create keyword
+--in Postgres, everything defaults to lowercase, unless you use double quotes
+
+/*create table users(
+	"USERNAME" text
+); 
+
+alter table users add user_age int;
+
+drop table users;*/
+
+--Not seen: truncate - drops all RECORDS in a table (we added no records here)
+
+--this is a very rudimentary table, it's a bit more complicated than this usually
+------------------------
+
+--Now let's talk about datatypes. Anybody know some types?
+
+--We won't actually use this table but it's good for practice :p
+create table datatypes (
+	numbers_small int2,
+	numbers_normal int,
+	numbers_also_normal int4,
+	numbers_large int8,
+	standard_number numeric(10, 2), --2 parameters: #digits, #decimals
+	
+	"boolean" boolean, --double quotes lets you use assign keywords to things
+	
+	fixed_length_text char(10), --will be 10 characters, not best practice
+	variable_length_text varchar(10), --from 0-10 characters, ok practice
+	unlimited_length_text text, --best practice, unlimited length
+	
+	"timestamp" timestamp --YYYY-MM-DD HH-MM-SS
+	
+	--This is NOT all of the datatypes. There are so many.
+	--But... most of the time data is just gonna be numbers and text.
+);
+
+
+
