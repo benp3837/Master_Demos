f82786f60c8b64ca18decbf133ccfadc011bb522
Benjamin Petruzziello <benjamin.petruzziello@revature.com>
2021-05-19 11:11:26 -0400

    HelloDOM

---
+++ HTML_CSS_JS/HelloJS/HelloDOM/HelloDOM.js
@@ -0,0 +1,22 @@
+//I want all my divs with the classname coolclassname.
+let divElements = document.getElementsByClassName("coolclassname");
+
+console.log(typeof divElements); //object
+
+//We can also access certain elements in the object
+let firstDiv = divElements[0];
+console.log(firstDiv); //the div's contents will print out
+
+//we can set attributes of elements, though it's not the best practice
+//there's an actual style object you can set properties of for every element
+firstDiv.setAttribute("style", "background-color:pink;");
+
+//now let's assign a variable that applies to all buttons
+let myButton = document.getElementsByTagName('button')[0];
+
+myButton.onclick = domFunc; //so now when a button is clicked, this function runs
+
+function domFunc(){
+    document.getElementById("b1").textContent = "Thanks for the click!";
+    document.getElementById("p3").textContent = "Now I have content!";
+}
