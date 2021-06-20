8662bac9ebda16e6a98ac8bd3ec3dbbb4b144df0
Benjamin Petruzziello <benjamin.petruzziello@revature.com>
2021-06-20 18:17:16 -0400

    Almost done with Todos

--- Angular/todo-list/src/app/app.module.ts
+++ Angular/todo-list/src/app/app.module.ts
@@ -3,16 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TodosBannerComponent } from './components/todos-banner/todos-banner.component';
 
+//This is the NgModule, which makes the various parts of our application aware of each other.
 @NgModule({
-  declarations: [
-    AppComponent
+  declarations: [ //declarations are "native" components that we created
+    AppComponent,
+    TodosBannerComponent
   ],
-  imports: [
+  imports: [ //imports are other modules that we'll use as dependencies
     BrowserModule,
     AppRoutingModule
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [], //providers are where we put services. (pure-logic TS files to give functionality).
+  bootstrap: [AppComponent] //The bootstrap is the first page returned to the user
+                            //So in this case, it's the HTML file of the AppComponent
 })
 export class AppModule { }
