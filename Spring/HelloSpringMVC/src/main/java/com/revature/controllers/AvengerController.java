ee2836770d10c07cea04e8e185a79ed939589eb0
Benjamin Petruzziello <benjamin.petruzziello@revature.com>
2021-07-20 13:16:48 -0400

    Finished HelloSpringMVC

--- Spring/HelloSpringMVC/src/main/java/com/revature/controllers/AvengerController.java
+++ Spring/HelloSpringMVC/src/main/java/com/revature/controllers/AvengerController.java
@@ -6,6 +6,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -21,6 +22,8 @@ import com.revature.models.Avenger;
 //@Controller (use this one before changing it to RestController)
 @RequestMapping(value="/avenger")
 //@ResponseBody //Adds the @ResponseBody to every method in the controller at compile time. 
+@CrossOrigin //This will act as a CORS filter, allowing all requests if no attributes 
+//are provided. You can specify specific origins.
 public class AvengerController {
 	
 	private AvengerDAO dao;
