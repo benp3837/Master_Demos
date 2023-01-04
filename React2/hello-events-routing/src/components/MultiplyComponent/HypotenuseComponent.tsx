c28ef098a44c602d34cc7a5bfc00b343c1244ee7
benp3837 <petruzziellbj@alumni.vcu.edu>
2023-01-04 17:52:35 -0500

    easier

--- React2/hello-events-routing/src/components/MultiplyComponent/HypotenuseComponent.tsx
+++ React2/hello-events-routing/src/components/MultiplyComponent/HypotenuseComponent.tsx
@@ -13,37 +13,38 @@ export const HypotenuseComponent: React.FC<any> = () => {
      let [userVals, setValues] = useState({first:0, second:0}) //yes we can use objects as default val!
      let [result, setResult] = useState(0) //set to zero by default
  
-     /* Declaring a function that takes in an object (of any type) to calculate hypotenuse.
-     This will be called every time a SideComponent changes (when a user inputs a side value).
+    const storeValues = (e:any) => {
+
+        //fill out the values object using our setValues mutator, with whatever user data comes in
+        //we'll take the name of the input, and give the value to the specified input (first or second)
+        setValues({
+            //remember we need "..." if we're referring to an object
+            ...userVals,
+
+            /* WHAT is this syntax? 
+            the value inserted will be assigned to either "first" or "second" in userVals... 
+            depending on which value the user inputs.
+            e.g. first:value OR second:value. IT'S VALUE ASSIGNMENT!!! */
+            [e.target.name]:e.target.value 
+        })
+    }
+
+
+     /* This will be called every time a SideComponent changes (when a user inputs a side value).
      within this function, we'll use our setValues mutator to set side values for calculation */
      const calculateResult = (e:any) => {
-         //fill out the values object using our setValues mutator, with whatever user data comes in
-         //we'll take the name of the input, and give the value to the specified input (first or second)
-         setValues(
-             {
-                 //remember we need "..." if we're referring to an object
-                 ...userVals,
- 
-                 /* WHAT is this syntax? 
-                 the value inserted will be assigned to either "first" or "second"... 
-                 depending on which value the user inputs.
-                 e.g. first:value OR second:value. IT'S VALUE ASSIGNMENT!!! */
-                 [e.target.name]:e.target.value 
-             }
-         )
- 
-     //multiple the user's inputs, and put the result into this resultCalc variable 
-     //(the square root of the sum of both sides to the second power)
+     //multiply the user's inputs, and put the result into the result state variable
      setResult(userVals.first * userVals.second)
- 
- }
+    }
 
     return(
         <div className="input-container">
             <h3>Multiply Two Numbers:</h3>
-            <SideComponent name="first" onChange={calculateResult}/>
-            <SideComponent name="second" onChange={calculateResult}/>
+            <SideComponent name="first" onChange={storeValues}/>
+            <SideComponent name="second" onChange={storeValues}/>
+            <br />
             <ResultComponent values={userVals} result={result}/>
+            <button onClick={calculateResult}>Calculate!</button>
         </div>
     )
 
--- React2/hello-events-routing/src/components/ResultComponent/ResultComponent.tsx
+++ React2/hello-events-routing/src/components/ResultComponent/ResultComponent.tsx
@@ -2,17 +2,17 @@ import React from "react"
 
 export const ResultComponent: React.FC<any> = (props:any) => {
 
-    /*
-        If the first and second value exist (if they're both truthy)
-        populate the <p> to contain the result
-        If there isn't a value for both the first and second, give the user a prompt to enter values
-    */
+    //If there isn't a value for both the first and second, give the user a prompt to enter values
+    //If there is no value for result, tell the user to click the button and trigger calculation
     return(
         <div className="result-container">
-            <p>
-                {props.values.first && props.values.second ? `Product is: ${props.result}`
-                : "Enter both numbers for a value"}
-            </p>
+            <p style={{color: "red"}}>{props.values.first && props.values.second ? '' 
+            : "Please enter both numbers"}</p>
+
+            <div>
+                {props.result ? <p> You calculated: {props.result}</p>
+                : "Click calculate to find the product:"}
+            </div>
         </div>
     )
 
