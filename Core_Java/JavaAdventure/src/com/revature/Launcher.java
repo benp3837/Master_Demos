2f07e6a08fcabb10fe066dfd7fb0e248e84d1b51
benp3837 <petruzziellbj@alumni.vcu.edu>
2023-06-16 12:08:42 -0400

    revamped Java Adventure

---
+++ Core_Java/JavaAdventure/src/com/revature/Launcher.java
@@ -0,0 +1,444 @@
+package com.revature;
+
+public class Launcher {
+
+    public static void main(String[] args) {
+
+        /* This is the beginning of a harrowing Java adventure -
+        -Create a Player Class by implementing the IPlayer Interface.
+        -All Players should have:
+            -name
+            -weapon = "No Weapon"
+            -armor = "No Armor"
+            -health = 100
+
+
+        -Instantiate your Player below, ONLY giving it a name
+        -Print out "[player name] was born!" */
+
+
+
+
+
+
+
+
+        /* Despite you just being born, you're surrounded by wolves!! Where are your parents?
+
+          -fight() them off!!!
+                _
+               / \      _-'
+             _/|  \-''- _ /
+        __-' { |          \
+            /             \
+            /       "o.  |o }
+            |            \ ;
+                          ',
+               \_         __\
+                 ''-_    \.//
+                   / '-____'
+                  /
+                _'
+              _-'*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+    /*You barely survive, losing 90 health in the fight
+     -Subtract 90 from player health, and print out remaining health*/
+
+
+
+
+
+
+
+
+
+
+
+
+    /* Welcome to the Aspirin merchant! Fighting wolves always leaves me with a headache...
+
+    "You may take some Aspirin by solving my riddle nyahahahaha" says the merchant.
+
+    "Declare an int that is not quite an int, but its OBJECT representation.
+     use it to assign your health to 200 - such is the Aspirin incantation"
+
+         _____
+        [IIIII]
+         )"""(
+        /     \
+       /       \
+       |`-...-'|
+       | asprin|
+     _ |`-...-'j    _
+    (\)`-.___.(I) _(/)
+      (I)  (/)(I)(\) */
+
+
+
+
+
+    //How refreshing!
+
+
+
+
+
+
+
+
+
+
+
+
+
+        /* Welcome to the blacksmith! It's time for some upgrades.
+        -Craft and assign yourself a weapon and armor using the craft() method⠀⠀⠀⠀
+        -Check out your new gear by printing it
+        ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣰⣿⣿⣷⡄⠀⠀⠀⠀⠀⠀⠀⢠⣄⣤⣦⣤⣀⣀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢿⣿⣿⣿⡇⠀⠀⠀⠀⠀⠀⠀⠀⠈⠉⠛⠿⠟⠈⠉
+⠀⠀⠀⠀⠀⠀⠀⠀⣠⠀⠘⢿⣿⠟⠀⢠⡀⠀⠀⠀⠀⠀⠀⠀⣰⡗⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⢠⣾⠀⣿⠀⣷⣦⣤⣴⡇⢸⡇⠀⣷⠀⠀⠀⠀⣰⡟⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⣿⣿⠀⣿⣤⣈⣉⣉⣉⣠⣼⡇⠀⣿⡆⠀⠀⣰⡟⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⣿⣿⠀⣿⣿⣿⣿⣿⣿⣿⣿⡇⠀⣿⠇⠀⠀⠛⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠛⠛⠀⠛⠛⠛⠛⠛⠛⠛⠛⠃⠀⠛⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⣤⣤⣤⣤⣤⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣇⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠈⠻⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣷⣤⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠈⠙⠛⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡿⠟⠋⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠿⠿⠿⣿⣿⣿⣿⣿⣿⣿⠿⠿⠿⠇⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣰⣿⣿⣿⣿⣿⣿⣿⣧⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⢀⣠⣾⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣦⡀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠘⠛⠛⠛⠛⠛⠛⠛⠛⠛⠛⠛⠛⠛⠛⠛⠛⠃*/
+
+
+
+
+
+
+
+
+
+
+
+
+    /*You encounter an angry Wizard! He looks like he's got unhandled problems.
+
+    "I pity you traveler, for you will be the new bearer of my burden... TRY to handle THIS!!!"
+
+    handle the Wizard's spell, and fight() back!
+
+         __/\__
+    . _   \''/
+    -( )-/_||_\
+     .'. \_()_/
+      |   | . \
+      |   | .  \
+     .'. ,\_____'. */
+
+       /*~~~BOOM~~~*/
+
+        int x = 2/0;
+
+
+
+
+
+
+
+
+
+
+
+
+
+    /* "WOW!! My curse has been lifted. I wish you didn't hit me back though.
+    Anyway, I will reward you if you can solve my riddle -
+
+    "Make me a sequence of 1, 2, and 3 - ordered and indexed this structure must be.
+    Print the second element of this creation - or else you shall suffer eternal damnation."
+         __/\__
+    . _   \^^/
+    -( )-/_||_\
+     .'. \_()_/
+      |   | . \
+      |   | .  \
+     .'. ,\_____'. */
+
+
+
+
+
+
+
+
+
+
+
+
+
+    /*After solving the wizard's riddle, he vanishes in a puff of bytecode!!
+      A chest lays at your feet! :O
+         _-_______-
+        /\____;;___\
+       | /         /
+       `. ())oo() .
+        |\(%()*^^()^\
+       %| |-%-------|
+      % \ | %  ))   |
+      %  \|%________|
+
+      -Iterate through the chest's loot
+      -Choose (print out) only the favorable items
+      -Skip the unfavorable items */
+
+      String[] items = {"Monster Energy", "Two dollars", "Two doll hairs", "Puppy", "The Goop",
+              "Cousin Timothy", "Three McChickens", "The Declaration of Independence",
+              "Staff of Plot Thickening"};
+
+
+
+
+
+
+
+
+
+
+
+
+
+      /*"The Staff of Plot Thickening? Better use this since I'm out of ideas for smooth transitions"
+
+      Upon using the Staff, an imp appears!! "Myahhh! I am an Imp named Slickback"
+
+        "Your name is Slickback?" You query.
+
+        "NO! My name is an Imp named Slickback. And for your impudence,
+        you must now answer my riddle lest I gnaw on your ankles!!"
+              ,      ,
+        |\   /(.-""-.)\ /|
+        | \ / =.  .= \ / |
+        \( \   o\/o   / )/
+         \_, '-/  \-' ,_/
+           /   \__/   \
+           \ \__/\__/ /
+            \ \|--|/ /
+
+        "What can we use, to let devs choose, datatypes during instantiation?
+        it's used in collections, and it's my perception that it lends to code flexibility"
+
+        *The imp is unaware that the riddle didn't rhyme*
+       */
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        /*Despite your correct answer, the imp betrayed you!! You're fighting!!!
+
+              O                                     O
+        {o)xxx|===============-  *  -===============|xxx(o}
+              O                                     O
+
+        -Uhhhh let's go with turn based combat. Okay yeah.
+        -While imp health is > 0, invoke fight() and subtract 20 from both fighter's health
+        -After the fight, print out your own health */
+
+        int impHealth = 100;
+
+
+
+
+
+
+
+
+
+
+
+
+        /*You're hurt, but the surgeon general recommends no more than 12 tablets of Aspirin
+        per 24 hours. Better wander into the nearest tavern for a drink instead.
+
+                            ________[_]________
+                   /\      /\        ______    \
+                  /  \    //_\       \    /\    \
+           /\    / /\/\  //___\       \__/  \    \
+          /  \  /\/    \//_____\       \ |[]|     \
+         /\/\/\/       //_______\       \|__|      \
+        /      \      /XXXXXXXXXX\                  \
+                \    /_I_II  I__I_\__________________\
+                       I_I|  I__I_____[]_|_[]_____I
+                       I_II  I__I_____[]_|_[]_____I
+                       I II__I  I     XXXXXXX     I
+
+        "How many riddles does a guy have to answer to get some mead?" You ask
+
+        "Just one, as usual!" Replies the innkeeper
+
+        "This non access mod makes beginners sob, so faint of heart beware...
+        It makes a variable belong to the Class, and its value between objects is shared" */
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        /* That wasn't so hard was it?
+
+                  )  (o
+                ( o  ) )
+                 ) ( (o
+                ooooooooo
+             .-'---ooooo-|
+            ( C|/\/\/oo\/|
+             '-./\/\/\o\/|
+               '_________'
+                '-------'
+
+        You're bubbly. It's time to dance!! Except you don't know how...
+
+        Implement a new method in the Player Class called dance() to learn how to dance!
+        -The dance method should take a String danceMove as a parameter. Call it below.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+                      _____
+        ⊂_ヽ          |    |
+             ＼＼    _|_____|_
+                 ＼( ͡° ͜ʖ ͡°)
+                      >    ⌒ヽ
+                    /      へ＼
+                 /        /    ＼＼
+                 ﾚ    ノ         ヽ_つ
+                /    /
+             /    /|
+            (    (ヽ
+            |    |、＼
+            | 丿 ＼ ⌒)
+            | |        ) /
+        ノ )        Lﾉ
+        (_／
+        you're magnificent.
+
+
+
+        The audience is getting involved!
+        They're shouting out an UNORDERED COLLECTION of dance moves with NO DUPLICATES!!
+
+        Ok audience... instantiate that Collection, and fill it with some dance moves.
+        For each dance move in the Collection, call the dance method!
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+         All this stimulation is HYPING YOU UP. Time for a good old fashioned bar brawl.
+
+         "HOW MANY OF YOU MILK DRINKERS WANNA CATCH SOME HANDS?" You inquire politely...
+
+         There were 10.
+
+         (ง'̀-'́)ง (ง'̀-'́)ง (ง'̀-'́)ง (ง'̀-'́)ง (ง'̀-'́)ง (ง'̀-'́)ง (ง'̀-'́)ง (ง'̀-'́)ง (ง'̀-'́)ง (ง'̀-'́)ง
+
+        You're getting absolutely stomped out by 10 tavern patrons! You've accepted your fate.
+
+        -Everyone is getting a hit in!
+        -For every patron, subtract 15 from your health.
+        -If your health is less than or equal to 0 after any hit, you die and the loop breaks! */
+
+
+
+
+
+
+
+
+
+
+
+
+
+     /* ███▀▀▀██┼███▀▀▀███┼███▀█▄█▀███┼██▀▀▀▀
+        ██┼┼┼┼██┼█┼┼┼┼┼┼┼█┼██┼┼┼█┼┼┼██┼██┼┼┼
+        ██┼┼┼▄▄▄┼██▄▄▄▄▄██┼██┼┼┼▀┼┼┼██┼██▀▀▀
+        ██┼┼┼┼██┼██┼┼┼┼┼██┼██┼┼┼┼┼┼┼██┼██┼┼┼
+        ███▄▄▄██┼██┼┼┼┼┼██┼██┼┼┼┼┼┼┼██┼██▄▄▄▄
+        ┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼
+        ███▀▀▀███┼▀███┼┼██▀┼██▀▀▀┼██▀▀▀▀██▄┼
+        ██┼┼┼┼┼██┼┼┼██┼┼██┼┼██┼┼┼┼██┼┼┼┼┼██┼
+        ██┼┼┼┼┼██┼┼┼██┼┼██┼┼██▀▀▀┼██▄▄▄▄▄▀▀┼
+        ██┼┼┼┼┼██┼┼┼██┼┼█▀┼┼██┼┼┼┼██┼┼┼┼┼██┼
+        ███▄▄▄███┼┼┼─▀█▀┼┼─┼██▄▄▄┼██┼┼┼┼┼██▄
+
+      O                                     O
+{o)xxx|===============-   *   -===============|xxx(o}
+      O                                     O           */
+
+
+
+
+
+
+
+
+
+    }
+
+}
