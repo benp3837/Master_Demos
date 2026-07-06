aebe679d1f9db2bd3753f7a6bb5df862ff6bc4eb
benp3837 <petruzziellbj@alumni.vcu.edu>
2026-07-06 15:30:06 -0400

    getting crabby

--- Angular/the-crab-store(hello-ang-2024)/src/app/components/buy/buy.component.ts
+++ Angular/the-crab-store(hello-ang-2024)/src/app/components/buy/buy.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { OrderService } from 'src/app/services/order.service';
 
 @Component({
   selector: 'app-buy',
@@ -7,14 +8,21 @@ import { Component } from '@angular/core';
 })
 export class BuyComponent {
 
+  //We need to CONSTRUCTOR INJECT the order service to use its data/functions
+  constructor(private orderService: OrderService){}
+
   //let's define some crabs that will be for sale when this component loads
   //in a real app, we'd probably pull this data from the backend/database
   crabList = [
-    {Type:"Bay Crab", Price:300},
-    {Type:"Horseshoe Crab", Price:3000},
-    {Type:"Spider Crab", Price:1000},
+    {ID:1, Type:"Bay Crab", Price:300},
+    {ID:2, Type:"Horseshoe Crab", Price:3000},
+    {ID:3, Type:"Spider Crab", Price:1000},
   ]
 
-  
+  //When a crab is clicked, add it to the cart in the OrderService
+  addItem(crab:any){
+      this.orderService.cart.push(crab)
+      alert("Added " + crab.Type)
+  }
 
 }
--- Angular/the-crab-store(hello-ang-2024)/src/app/components/cart/cart.component.ts
+++ Angular/the-crab-store(hello-ang-2024)/src/app/components/cart/cart.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { OrderService } from 'src/app/services/order.service';
 
 @Component({
   selector: 'app-cart',
@@ -6,5 +7,32 @@ import { Component } from '@angular/core';
   styleUrls: ['./cart.component.css']
 })
 export class CartComponent {
+
+  // We need to CONSTRUCTOR INJECT the order service to use its data/functions
+  constructor(public orderService: OrderService){}
+
+  /*Computed display-friendly view of the raw cart
+  "get" is SUPER useful cuz it lets us use this function as if it's a variable
+  See how we call it in the HTML*/
+ get cartSummary(): any {
+  const counts: any = {};
+  for (let crab of this.orderService.cart) {
+    if (counts[crab.Type]) {
+      counts[crab.Type].quantity++;
+    } else {
+      counts[crab.Type] = { ...crab, quantity: 1 };
+    }
+  }
+  return Object.values(counts);
+}
+
+
+  deleteItem(){
+    
+  }
+
+  makePurchase(){
+
+  }
 
 }
